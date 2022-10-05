911241e3cc36636849635f201f6915c3f3b30274
Raul B <r.au.l@outlook.com>
2022-10-05 23:40:06 +0200

    Login logic in place

--- scripts/login_page.js
+++ scripts/login_page.js
@@ -1,38 +1,110 @@
 const newUserBtn = document.getElementById("new_user");
 const loginBtn = document.getElementById("login_btn");
-const loginModal = document.getElementsByClassName("container");
+const loginModal = document.querySelector(".container");
 const loginForm = document.getElementById("my_form");
+const inputUsernameElement= document.getElementById("username");
+const inputPasswordElement= document.getElementById("password");
+
 const newUserForm = 
 `<h1>New user</h1><form action="#" method="post" id="my_from">
 <input type="text" placeholder="Choose username" class="field">
 <input type="password" placeholder="Choose password" class="field">
 <input type="submit" value="Create user" class="btn">
 </form> `;
+const logoutOption = 
+    `<div class="navbar_options">
+    <a href="" id="log_out">Logout</a>
+    </div>` 
 
-let fredrik = {
+const fredrik = {
     username: "fredrik",
     password: "12345",
     loggedIn: "false"
 }
-window.localStorage.setItem('fredrik', JSON.stringify(fredrik));
+const benicio = {
+    username: "benicio",
+    password: "456",
+    loggedIn: "false"
+}
+const arnold = {
+    username: "arnold",
+    password: "789",
+    loggedIn: "false"
+}
+const users = [fredrik, benicio, arnold];
 
-let jsonFredde13 = JSON.parse(window.localStorage.getItem('fredrik'));
-console.log(jsonFredde13);
+let logOutBtn;
+let currentUser = window.localStorage.getItem('current_user');
+
+for (const user of users){
+    window.localStorage.setItem(user.username, JSON.stringify(user));
+}
+
+if (currentUser){ 
+    //If we have a current in memory then we display the logged in view for the user   
+    let cu =  JSON.parse(currentUser);
+    changeNavBarToLoggedInUserOption(logoutOption);    
+    changeLoginModalToLoggedInUser(cu, loginModal);
+
+    logOutBtn = document.getElementById("log_out");
+    logOutBtn.addEventListener("click", logOutUser);
+}
+
+function changeLoginModalToLoggedInUser(currentUser, loginM=loginModal, loginF=loginForm){
+    /**
+     * Changes the login modal to the welcome user view
+     */
+    loginM.removeChild(loginF);    
+    const loggedInUserH1 = `<h1>Welcome ${currentUser.username}</h1>`;
+    loginM.innerHTML = loggedInUserH1;
+}
+
+function changeNavBarToLoggedInUserOption(logoutO=logoutOption){
+    /**
+     * Changes the nav bar to have to options for logged in users
+     */
+    const nav = document.querySelector("nav");
+    const nav_option = document.querySelector(".navbar_options");    
+    nav.removeChild(nav_option);    
+    nav.innerHTML = logoutO;
+}
+
+function logOutUser(loginF=loginForm){
+    /**
+     * Logs out user by removing them from local storage memory and displaying the login modal
+     */
+    window.localStorage.removeItem("current_user");
+    loginModal.innerHTML = loginF;    
+}
+
+loginBtn.addEventListener("click", function(){
+    let inputUsername= inputUsernameElement.value;
+    let inputPassword= inputPasswordElement.value;
+    console.log(`the inputUsername is: ${inputUsername}`);
+
+    let foundUser = window.localStorage.getItem(inputUsername);
+    if (!foundUser){
+        inputUsernameElement.style.border = "thick solid #f70a02ce"
+        return
+    }
+    let userObject = JSON.parse(foundUser);
+    let correctPass = userObject.password == inputPassword;
+
+    if (!correctPass){
+        inputPasswordElement.style.border = "thick solid #f70a02ce"
+        return
+    }
+
+    window.localStorage.setItem('current_user', JSON.stringify(userObject));
+
+    changeLoginModalToLoggedInUser(userObject)
+    changeNavBarToLoggedInUserOption()
+
+    logOutBtn = document.getElementById("log_out"); 
+    logOutBtn.addEventListener("click", logOutUser);
+});
 
 newUserBtn.addEventListener("click", function(){
     loginModal.removeChild(loginForm);
     loginModal.innerHTML = newUserForm;
-})
-
-loginBtn.addEventListener("click", function(){
-    let inputUsername= document.getElementById("username").value;
-    let inputPassword= document.getElementById("password").value;
-    console.log(`the inputUsername is: ${inputUsername}`);
-    let foundUser = window.localStorage.getItem(inputUsername);
-    if (foundUser == null){
-        console.log(`No user with that user name ${inputUsername}`);
-    }
-    else if (foundUser){
-        console.log("Found the user!")
-    }
-})
+});
