c5d569ec7f8c9fad583dbfa1f00d79a1b1084ae6
r-4u-1 <r.au.1@outlook.com>
2022-10-08 20:57:56 +0200

    New views wip

--- scripts/login_page.js
+++ scripts/login_page.js
@@ -1,22 +1,46 @@
+const navNewUserBtn = document.getElementById("nav_new_user");
+const navLoginBtn = document.getElementById("login");
 const newUserBtn = document.getElementById("new_user");
+const navbarElement = document.querySelector("nav");
+const navOptionElement = document.querySelector(".navbar_options"); 
 const loginBtn = document.getElementById("login_btn");
-const loginModal = document.querySelector(".container");
-const loginForm = document.getElementById("my_form");
+const containerFormElement = document.querySelector(".container");
+const formElement = document.getElementById("my_form");
 const inputUsernameElement= document.getElementById("username");
 const inputPasswordElement= document.getElementById("password");
 const validUserNameChars = "qwertyuiopåasdfghjklöäzxcvbnmQWERTYUIOPÅASDFGHJKLÖÄZXCVBNM";
 const validPasswordChars = "qwertyuiopåasdfghjklöäzxcvbnmQWERTYUIOPÅASDFGHJKLÖÄZXCVBNM1234567890";
 
 const newUserForm = 
-`<h1>New user</h1><div id="my_form">
-<input type="text" placeholder="Choose username" id="c_username" class="field">
-<input type="password" placeholder="Choose password" id="c_password" class="field">
-<input type="submit" value="Create user" id="createBtn" class="btn">
-</div> `;
-const logoutOption = 
+    `<h1>New user</h1><div id="my_form">
+    <input type="text" placeholder="Choose username" id="c_username" class="field">
+    <input type="password" placeholder="Choose password" id="c_password" class="field">
+    <input type="submit" value="Create user" id="createBtn" class="btn">
+    </div> `;
+
+const loginForm = `
+    <h1>Login</h1>
+    <div id="my_form">
+        <input type="text" placeholder="Username" class="field" id="username">
+        <input type="password" placeholder="Password" class="field" id="password">
+        <button class="btn" id="login_btn">Login</button>
+    </div>
+    `;
+const navLogoutOption = 
     `<div class="navbar_options">
-    <a href="" id="log_out">Logout</a>
-    </div>` 
+        <div id="logout">Logout</div>
+    </div>`
+
+const navLoginOption = 
+    `<div class="navbar_options">
+        <div id="login">Login</div>
+    </div>`;
+
+const navCreateUserOption = `
+<div class="navbar_options">
+    <div id="nav_new_user">Create a new user</div>
+</div>
+`;
 
 const fredrik = {
     username: "fredrik",
@@ -42,14 +66,19 @@ for (const user of users){
 if (currentUser){ 
     //If we have a current in memory then we display the logged in view for the user   
     let cu =  JSON.parse(currentUser);
-    changeNavBarToLoggedInUserOption(logoutOption);    
-    changeLoginModalToLoggedInUser(cu, loginModal);
+    changeNavbar(navLogoutOption);    
+    changeLoginModalToLoggedInUser(cu, containerFormElement);
 
     logOutBtn = document.getElementById("log_out");
     logOutBtn.addEventListener("click", logOutUser);
 }
 
-function changeLoginModalToLoggedInUser(currentUser, loginM=loginModal, loginF=loginForm){
+function changeModal(newOption, currentOption, modal=containerFormElement){
+    modal.removeChild(currentOption);    
+    modal.innerHTML = newOption;
+}
+
+function changeLoginModalToLoggedInUser(currentUser, loginM=containerFormElement, loginF=formElement){
     /**
      * Changes the login modal to the welcome user view
      */
@@ -58,56 +87,31 @@ function changeLoginModalToLoggedInUser(currentUser, loginM=loginModal, loginF=l
     loginM.innerHTML = loggedInUserH1;
 }
 
-function changeNavBarToLoggedInUserOption(logoutO=logoutOption){
+function changeNavbar(newOption=navLogoutOption, nav=navbarElement, currentOption=navOptionElement){
     /**
-     * Changes the nav bar to have to options for logged in users
+     * Changes the navbar to different views/options
      */
-    const nav = document.querySelector("nav");
-    const nav_option = document.querySelector(".navbar_options");    
-    nav.removeChild(nav_option);    
-    nav.innerHTML = logoutO;
+    nav.removeChild(currentOption);    
+    nav.innerHTML = newOption;
 }
 
-function logOutUser(loginF=loginForm){
+function logOutUser(loginF=formElement){
     /**
      * Logs out user by removing them from local storage memory and displaying the login modal
      */
     window.localStorage.removeItem("current_user");
-    loginModal.innerHTML = loginF;    
+    containerFormElement.innerHTML = loginF;    
 }
 
-loginBtn.addEventListener("click", function(){
-    let inputUsername= inputUsernameElement.value;
-    let inputPassword= inputPasswordElement.value;
-    console.log(`the inputUsername is: ${inputUsername}`);
+function changeModalToLoginView(){
+    changeNavbar(navCreateUserOption)
+    changeModal(loginForm, formElement)
+}
 
-    let foundUser = window.localStorage.getItem(inputUsername);
-    if (!foundUser){
-        inputUsernameElement.style.border = "thick solid #f70a02ce"
-        alert(`username: ${inputUsername} is not found.`);
-        return
-    }
-    let userObject = JSON.parse(foundUser);
-    let correctPass = userObject.password == inputPassword;
-
-    if (!correctPass){
-        inputPasswordElement.style.border = "thick solid #f70a02ce"
-        alert(`password: ${inputPassword} is incorrect.`);
-        return
-    }
-
-    window.localStorage.setItem('current_user', JSON.stringify(userObject));
-
-    changeLoginModalToLoggedInUser(userObject)
-    changeNavBarToLoggedInUserOption()
-
-    logOutBtn = document.getElementById("log_out"); 
-    logOutBtn.addEventListener("click", logOutUser);
-});
-
-newUserBtn.addEventListener("click", function(){
-    loginModal.removeChild(loginForm);
-    loginModal.innerHTML = newUserForm;
+function changeModalToNewUser(){
+    changeNavbar(navLoginOption)
+    containerFormElement.removeChild(formElement);
+    containerFormElement.innerHTML = newUserForm;
     const creatBtn = document.getElementById("createBtn");
 
     creatBtn.addEventListener("click", function(){
@@ -145,10 +149,43 @@ newUserBtn.addEventListener("click", function(){
             window.localStorage.setItem(newUser.username, JSON.stringify(newUser));
             window.localStorage.setItem('current_user', JSON.stringify(newUser));
             const newUserForm = document.getElementById("my_form");
-            changeLoginModalToLoggedInUser(newUser, loginModal, newUserForm);
-            changeNavBarToLoggedInUserOption();
+            changeLoginModalToLoggedInUser(newUser, containerFormElement, newUserForm);
+            changeNavbar();
             logOutBtn = document.getElementById("log_out"); 
             logOutBtn.addEventListener("click", logOutUser);
         }
     });
+}
+
+loginBtn.addEventListener("click", function(){
+    let inputUsername= inputUsernameElement.value;
+    let inputPassword= inputPasswordElement.value;
+    console.log(`the inputUsername is: ${inputUsername}`);
+
+    let foundUser = window.localStorage.getItem(inputUsername);
+    if (!foundUser){
+        inputUsernameElement.style.border = "thick solid #f70a02ce"
+        alert(`username: ${inputUsername} is not found.`);
+        return
+    }
+    let userObject = JSON.parse(foundUser);
+    let correctPass = userObject.password == inputPassword;
+
+    if (!correctPass){
+        inputPasswordElement.style.border = "thick solid #f70a02ce"
+        alert(`password: ${inputPassword} is incorrect.`);
+        return
+    }
+
+    window.localStorage.setItem('current_user', JSON.stringify(userObject));
+
+    changeLoginModalToLoggedInUser(userObject)
+    changeNavbar()
+
+    logOutBtn = document.getElementById("log_out"); 
+    logOutBtn.addEventListener("click", logOutUser);
 });
+
+newUserBtn.addEventListener("click", changeModalToNewUser);
+navNewUserBtn.addEventListener("click", changeModalToNewUser);
+navLoginBtn.ad
